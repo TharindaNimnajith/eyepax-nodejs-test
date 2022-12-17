c66495117e24e45af3fcc7c132d1a9782bd867c2
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2022-12-17 16:05:42 +0530

    get, put, delete methods

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -7,26 +7,125 @@ const addEmployee = async (req, res) => {
     status
   } = req.body
 
-  const newEmployee = new EmployeeModel({
+  const employee = new EmployeeModel({
     name,
     email,
     status
   })
 
   try {
-    await newEmployee.save()
-    console.log('New employee added successfully.')
+    await employee.save()
     res.status(201).send({
       status: 201,
-      message: 'New employee added successfully.'
+      data: employee
     })
   } catch (error) {
     console.error(error)
     res.status(400).send({
       status: 400,
-      message: error
+      error: error
+    })
+  }
+}
+
+const updateEmployee = async (req, res) => {
+  let employee
+
+  const {
+    name,
+    email,
+    status
+  } = req.body
+
+  try {
+    employee = await EmployeeModel.findOne({
+      email: email
+    })
+    employee.name = name
+    employee.status = status
+    await employee.save()
+    res.status(200).send({
+      status: 200,
+      data: employee
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(400).send({
+      status: 400,
+      error: error
+    })
+  }
+}
+
+const deleteEmployee = async (req, res) => {
+  let employee
+
+  const {
+    email
+  } = req.body
+
+  try {
+    employee = await EmployeeModel.findOne({
+      email: email
+    })
+    await employee.remove()
+    res.status(202).send({
+      status: 202,
+      data: employee
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(404).send({
+      status: 404,
+      error: error
+    })
+  }
+}
+
+const getEmployee = async (req, res) => {
+  let employee
+
+  const {
+    email
+  } = req.params
+
+  try {
+    employee = await EmployeeModel.findOne({
+      email: email
+    })
+    res.status(200).send({
+      status: 200,
+      data: employee
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(404).send({
+      status: 404,
+      error: error
+    })
+  }
+}
+
+const getEmployeeList = async (req, res) => {
+  let employees
+
+  try {
+    employees = await EmployeeModel.find()
+    res.status(200).send({
+      status: 200,
+      data: employees
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(404).send({
+      status: 404,
+      error: error
     })
   }
 }
 
 exports.addEmployee = addEmployee
+exports.updateEmployee = updateEmployee
+exports.deleteEmployee = deleteEmployee
+exports.getEmployee = getEmployee
+exports.getEmployeeList = getEmployeeList
--- routes/employee.routes.js
+++ routes/employee.routes.js
@@ -5,5 +5,9 @@ const EmployeeController = require('../controllers/employee.controller')
 const router = express.Router()
 
 router.post('/employees', EmployeeController.addEmployee)
+router.put('/employees', EmployeeController.updateEmployee)
+router.delete('/employees', EmployeeController.deleteEmployee)
+router.get('/employees/:email', EmployeeController.getEmployee)
+router.get('/employees', EmployeeController.getEmployeeList)
 
 module.exports = router
